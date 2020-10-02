22364a724a1d8f8136b70e66b4d0a3b89d4ea65b
mztikk <nevigskcuforez@gmail.com>
2020-10-02 19:40:37 +0200

    implemented IEquatable for BackupItem

--- BackBack.Models/BackupItem.cs
+++ BackBack.Models/BackupItem.cs
@@ -2,7 +2,7 @@
 
 namespace BackBack.Models
 {
-    public class BackupItem
+    public class BackupItem : IEquatable<BackupItem>
     {
         public string Name { get; set; }
         public string Source { get; set; }
@@ -16,5 +16,7 @@ namespace BackBack.Models
         public DateTime LastExecution { get; set; }
         public bool BackupPeriodically { get; set; }
         public TimeSpan Interval { get; set; }
+
+        public bool Equals(BackupItem other) => other.Name.Equals(Name);
     }
 }
