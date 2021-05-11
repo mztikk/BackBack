e13f0986b2181b56da88471d4d0c79d936576445
Nils Feierabend <mztikk@outlook.de>
2021-05-11 22:11:41 +0200

    added another backup item to test

--- BackBack/ViewModels/BackupItemsViewModel.cs
+++ BackBack/ViewModels/BackupItemsViewModel.cs
@@ -9,9 +9,13 @@ namespace BackBack.ViewModels
         {
             //Items.Add(new() { Name = "REPOS" });
             var dto = new BackupItem() { Name = "REPOS", Source = "E:/repos", Target = "D:/backups/reposbackup" };
+            var dto2 = new BackupItem() { Name = "SCRIPTS", Source = "E:/scripts", Target = "D:/backups/scriptsbackup" };
             var item = new BackupItemViewModel();
+            var item2 = new BackupItemViewModel();
             PropertySync.Sync(dto, item);
+            PropertySync.Sync(dto2, item2);
             Items.Add(item);
+            Items.Add(item2);
         }
         public ObservableCollection<BackupItemViewModel> Items { get; } = new();
     }
