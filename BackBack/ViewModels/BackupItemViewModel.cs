19a240596b6a62a73372981fddbd2c16a08a1bf0
Nils Feierabend <mztikk@outlook.de>
2021-05-11 23:07:39 +0200

    added top buttons, made backupitems selectable

--- BackBack/ViewModels/BackupItemViewModel.cs
+++ BackBack/ViewModels/BackupItemViewModel.cs
@@ -1,15 +1,27 @@
-﻿using ReactiveUI;
+﻿using System.Windows.Input;
+using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
 
 namespace BackBack.ViewModels
 {
     public class BackupItemViewModel : ViewModelBase
     {
+        public BackupItemViewModel()
+        {
+            PressedCommand = ReactiveCommand.Create(Pressed);
+        }
+
         [Reactive]
         public string? Name { get; set; }
         [Reactive]
         public string? Source { get; set; }
         [Reactive]
         public string? Target { get; set; }
+        [Reactive]
+        public bool Selected { get; set; }
+        [Reactive]
+        public ReactiveCommand<System.Reactive.Unit, System.Reactive.Unit> PressedCommand { get; set; }
+
+        private void Pressed() => System.Console.WriteLine("pressed");
     }
 }
--- BackBack/Views/BackupItemView.axaml.cs
+++ BackBack/Views/BackupItemView.axaml.cs
@@ -1,19 +1,22 @@
-using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
+using BackBack.ViewModels;
 
 namespace BackBack.Views
 {
     public partial class BackupItemView : UserControl
     {
-        public BackupItemView()
-        {
-            InitializeComponent();
-        }
+        public BackupItemView() => InitializeComponent();
 
-        private void InitializeComponent()
+        private void InitializeComponent() => AvaloniaXamlLoader.Load(this);
+
+        public void PointerPressed(object sender, PointerPressedEventArgs e)
         {
-            AvaloniaXamlLoader.Load(this);
+            if (DataContext is BackupItemViewModel vm)
+            {
+                vm.Selected = !vm.Selected;
+            }
         }
     }
 }
