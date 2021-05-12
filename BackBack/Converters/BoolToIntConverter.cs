dbbcdef3cd9fb8c5e202861d76657da0ebbe7352
Nils Feierabend <mztikk@outlook.de>
2021-05-12 21:55:03 +0200

    added booltoint and booltothickness converter

---
+++ BackBack/Converters/BoolToIntConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia.Data.Converters;
+
+namespace BackBack.Converters
+{
+    public class BoolToIntConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool b && (parameter is int i || (parameter is string s && int.TryParse(s, out i))))
+            {
+                return b ? i : 0;
+            }
+
+            throw new ArgumentException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
