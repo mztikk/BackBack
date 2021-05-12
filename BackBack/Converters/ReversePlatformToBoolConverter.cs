ef55a2a6f603e272441fbf9e9a03d524841fc9f1
Nils Feierabend <mztikk@outlook.de>
2021-05-13 00:40:06 +0200

    added Reverse/PlatformToBoolConverter

---
+++ BackBack/Converters/ReversePlatformToBoolConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using Avalonia.Data.Converters;
+
+namespace BackBack.Converters
+{
+    public class ReversePlatformToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is OSPlatform p)
+            {
+                return !RuntimeInformation.IsOSPlatform(p);
+            }
+
+            throw new ArgumentException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
